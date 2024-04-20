4c4c1e6976d3f3fd84f700ae9a062d73d3e4e098
wwwDayDream <daymonforeman14@gmail.com>
2024-04-20 13:42:35 -0500

    Working state

--- CessilCellsCeaChells/CessilCellsCeaChellsDownByTheCeaChore.cs
+++ CessilCellsCeaChells/CessilCellsCeaChellsDownByTheCeaChore.cs
@@ -4,55 +4,10 @@ using System.IO;
 using System.Linq;
 using BepInEx;
 using BepInEx.Logging;
-using CessilCellsCeaChells.CeaChore;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 
 namespace CessilCellsCeaChells;
-
-public class Merge {
-    public enum MergeType { Field, Property, Method }
-    public TypeReference TargetTypeReference;
-    public string TargetAssemblyName => TargetTypeReference.Resolve().Module.Assembly.Name.Name + ".dll";
-    
-    public string Identifer;
-    public TypeReference IdType;
-    public MergeType Type;
-
-    public TypeReference[] AdditionalTypeParameters = [];
-
-    public Merge(CustomAttribute attribute)
-    {
-        TargetTypeReference = (TypeReference)attribute.ConstructorArguments[0].Value;
-        Identifer = (string)attribute.ConstructorArguments[1].Value;
-        IdType = (TypeReference)attribute.ConstructorArguments[2].Value;
-        
-        if (attribute.AttributeType.FullName == typeof(RequiresFieldAttribute).FullName)
-        {
-            Type = MergeType.Field;
-        } else if (attribute.AttributeType.FullName == typeof(RequiresPropertyAttribute).FullName)
-        {
-            Type = MergeType.Property;
-        } else if (attribute.AttributeType.FullName == typeof(RequiresMethodAttribute).FullName)
-        {
-            Type = MergeType.Method;
-            AdditionalTypeParameters = ((CustomAttributeArgument[])attribute.ConstructorArguments[3].Value)
-                .Select(attr => (TypeReference)attr.Value).ToArray();
-        }
-    }
-    
-    public static bool TryCreateMerges(AssemblyDefinition assemblyDefinition, out Merge[] merges)
-    {
-        merges = assemblyDefinition.CustomAttributes
-            .Where(attr =>
-                attr.AttributeType.FullName == typeof(RequiresFieldAttribute).FullName ||
-                attr.AttributeType.FullName == typeof(RequiresPropertyAttribute).FullName ||
-                attr.AttributeType.FullName == typeof(RequiresMethodAttribute).FullName)
-            .Select(attr => new Merge(attr)).ToArray();
-        
-        return merges.Length > 0;
-    }
-}
 
 public static class CessilCellsCeaChellsDownByTheCeaChore {
     public static IEnumerable<string> TargetDLLs => Merges.Select(merge => merge.TargetAssemblyName).Distinct();
