4d6e8bda78f7db5390630a08abe47bf72282723f
wwwDayDream <daymonforeman14@gmail.com>
2024-04-23 23:31:03 -0500

    Merged restructure from feat_type-editing & Removed Type Editing

--- CessilCellsCeaChells/CeaChore/Attributes.cs
+++ CessilCellsCeaChells/CeaChore/Attributes.cs
@@ -10,12 +10,3 @@ public class RequiresPropertyAttribute(Type targetType, string propertyName, Typ
 public class RequiresMethodAttribute(Type targetType, string methodName, Type returnType, params Type[] arguments) : RequiresAttribute;
 
 public class RequiresMethodDefaultsAttribute(Type targetType, string methodName, Type returnType, Type[] argumentTypes, object[] argumentDefaults) : RequiresAttribute;
-
-[Flags]
-public enum FieldChange { FieldType = 1 }
-[Flags]
-public enum PropertyChange { PropertyType = 1, AddSetter = 2, AddGetter = 4, AddSingletonGetter = 8 }
-
-public class RequiresFieldChangeAttribute(Type targetType, string fieldName, FieldChange modificationType, Type? newType = null) : RequiresAttribute;
-
-public class RequiresPropertyChangeAttribute(Type targetType, string propertyName, PropertyChange modificationType, Type? newType = null) : RequiresAttribute;
