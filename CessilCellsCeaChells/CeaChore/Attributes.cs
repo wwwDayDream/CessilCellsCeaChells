bc2f5eec76f8045a31df23d35762f8a81b645062
wwwDayDream <daymonforeman14@gmail.com>
2024-04-28 05:52:44 -0500

    Reformatted Attributes.cs

--- CessilCellsCeaChells/CeaChore/Attributes.cs
+++ CessilCellsCeaChells/CeaChore/Attributes.cs
@@ -5,29 +5,17 @@ using JetBrains.Annotations;
 namespace CessilCellsCeaChells.CeaChore;
 
 public class RequiresFieldAttribute : RequiresAttribute {
-    [UsedImplicitly]
-    public RequiresFieldAttribute(Type targetType, string fieldName, Type fieldType)
-    {
-    }
+    [UsedImplicitly] public RequiresFieldAttribute(Type targetType, string fieldName, Type fieldType) {}
 }
 
 public class RequiresPropertyAttribute : RequiresAttribute {
-    [UsedImplicitly]
-    public RequiresPropertyAttribute(Type targetType, string propertyName, Type propertyType, bool singletonCreateOnAccess = false)
-    {
-    }
+    [UsedImplicitly] public RequiresPropertyAttribute(Type targetType, string propertyName, Type propertyType, bool singletonCreateOnAccess = false) {}
 }
 
 public class RequiresMethodAttribute : RequiresAttribute {
-    [UsedImplicitly]
-    public RequiresMethodAttribute(Type targetType, string methodName, Type returnType, params Type[] arguments)
-    {
-    }
+    [UsedImplicitly] public RequiresMethodAttribute(Type targetType, string methodName, Type returnType, params Type[] arguments) {}
 }
 
 public class RequiresMethodDefaultsAttribute : RequiresAttribute {
-    [UsedImplicitly]
-    public RequiresMethodDefaultsAttribute(Type targetType, string methodName, Type returnType, Type[] argumentTypes, object[] argumentDefaults)
-    {
-    }
+    [UsedImplicitly] public RequiresMethodDefaultsAttribute(Type targetType, string methodName, Type returnType, Type[] argumentTypes, object[] argumentDefaults) {}
 }
